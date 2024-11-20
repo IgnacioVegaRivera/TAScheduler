89c444fb3edef936f426e995db543054257550e4
maxk100 <maxkrug@uwm.edu>
2024-11-20 16:38:12 -0600

    basic models of course and lab

--- djangoProject1/models.py
+++ djangoProject1/models.py
@@ -13,6 +13,7 @@ class User(models.Model): # basic for now
     def __str__(self):
         return self.name
 
+# test test
 class Course(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
