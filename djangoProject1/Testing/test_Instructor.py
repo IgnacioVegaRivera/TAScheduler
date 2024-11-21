4c9b7b74f5542a5710acec7849cb97fdc44a28c6
MatthewMaijala <mmaijala@uwm.edu>
2024-11-21 14:19:56 -0600

    Created test files for each separate User Methods file

---
+++ djangoProject1/Testing/test_Instructor.py
@@ -0,0 +1,21 @@
+# from Instructor import instructor
+from django.test import TestCase, Client
+import unittest
+from unittest.mock import Mock
+
+# class UnitInstructorTest(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class AcceptanceInstructorTest(unittest.TestCase):
---
+++ djangoProject1/Testing/test_TA.py
@@ -0,0 +1,21 @@
+# from TA import ta
+from django.test import TestCase, Client
+import unittest
+from unittest.mock import Mock
+
+# class UnitTATest(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class AcceptanceTATest(unittest.TestCase):
