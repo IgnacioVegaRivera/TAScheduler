eeb6ccd3d48b29e681b003a23924467ba923ec94
SMaillis <smaillis@uwm.edu>
2024-11-21 11:58:43 -0600

    Fixed the issue with it not redirecting properly from the login page. hopefully the post method commits this time :/

--- djangoProject1/models.py
+++ djangoProject1/models.py
@@ -1,22 +1,6 @@
 from django.db import models
-import uuid
+
 
 class User(models.Model): # basic for now
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
-    # email = models.CharField(max_length=100)
-    # phoneNumber = models.CharField(max_length=100)
-    # userName = models.CharField(max_length=100)
-    # passWord = models.CharField(max_length=100)
-    # address = models.CharField(max_length=100)
-
-    def str(self):
-        return self.name
-
-class Course(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
-
-class Lab(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
