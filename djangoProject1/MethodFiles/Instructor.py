234157806ebfd997797a0d3f2387a73fa5ac9b02
IgnacioVegaRivera <vegariv2@uwm.edu>
2024-12-14 17:12:15 -0600

    created the interface and method stub for assign_user

--- djangoProject1/MethodFiles/Instructor.py
+++ djangoProject1/MethodFiles/Instructor.py
@@ -1,1 +1,7 @@
-# file for Instructor methods
+from djangoProject1.MethodFiles.Interfaces import AssignUserInterface
+from djangoProject1.models import User, Course, Section
+
+class AssignUser(AssignUserInterface):
+    @staticmethod
+    def assign_user(section_id, user, request):
+        pass
