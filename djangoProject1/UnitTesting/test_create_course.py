ef2fc8525acbacf4313e0a4b7fe1969b1786efc7
SMaillis <smaillis@uwm.edu>
2024-12-16 15:55:11 -0600

    Unit Tests for Create Course

--- djangoProject1/UnitTesting/test_create_course.py
+++ djangoProject1/UnitTesting/test_create_course.py
@@ -8,91 +8,56 @@ class CreateCourseUnitTest(TestCase):
     def setUp(self):
         self.user = User(first_name="(no", last_name="instructor)", role="Instructor")
         self.user.save()
+        self.ta = User(role="TA")
+        self.ta.save()
 
     #to test properly we must save within the helper method
     def test_create_course(self):
-        course = CreateCourse.create_course("name", self.user)
+        course = CreateCourse.create_course("name", [self.user])
         self.assertEqual(course.name,"name")
-        self.assertEqual(course.users.first(), self.user)
+        self.assertEqual(course.users, [self.user])
 
     #if anything is wrong then we want to set course to none and don't save it to the database
     def test_no_name(self):
-        course = CreateCourse.create_course("", self.user)
+        course = CreateCourse.create_course("", [self.user])
         self.assertEqual(course, None)
 
     def test_no_instructor(self):
-        course = CreateCourse.create_course("name", None)
-        self.assertEqual(course, None)
+        course = CreateCourse.create_course("name", [])
+        self.assertEqual(course.name, "name")
+        self.assertEqual(course.users, [])
 
-    def test_not_an_instructor(self):
-        ta = User(role="TA")
-        course = CreateCourse.create_course("name", ta)
-        self.assertEqual(course, None)
+    def test_adding_ta(self):
+
+        course = CreateCourse.create_course("name", [self.ta])
+        self.assertEqual(course.name, "name")
+        self.assertEqual(course.users, [self.ta])
 
     def test_invalid_type(self):
-        course = CreateCourse.create_course(123, self.user)
+        course = CreateCourse.create_course(123, [self.user])
         self.assertEqual(course, None)
 
     def test_invalid_type_again(self):
-        course = CreateCourse.create_course("name", "user")
+        course = CreateCourse.create_course("name", ["user"])
         self.assertEqual(course, None)
 
     def test_course_already_exists(self):
-        course = CreateCourse.create_course("name", self.user)
+        course = CreateCourse.create_course("name", [self.user])
         self.assertEqual(course.name, "name")
-        self.assertEqual(course.users.first(), self.user)
+        self.assertEqual(course.users.first(), [self.user])
 
-        course2 = CreateCourse.create_course("name", self.user)
+        course2 = CreateCourse.create_course("name", [self.user])
         self.assertEqual(course2, None)
 
+    def test_course_add_two_users(self):
+        course = CreateCourse.create_course("name", [self.ta, self.user])
+        self.assertEqual(course.name, "name")
+        self.assertEqual(course.users.first(), [self.ta, self.user])
 
+    def test_course_one_valid_one_invalid_user(self):
+        course = CreateCourse.create_course("name", [self.ta, "instructor"])
+        self.assertEqual(course, None)
 
-# class CreateCourseAcceptanceTest(TestCase):
-#     def setUp(self):
-#         self.donkey = Client()
-#         self.valid = User(username="valid", first_name="(no", last_name="instructor)", role="Instructor")
-#         self.invalid = User(role="TA")
-#         self.valid.save()
-#         self.invalid.save()
-#
-#     #test if the inputted course is valid then it is added to the database
-#     def test_valid_course(self):
-#         response = self.donkey.post("/configure_course.html",
-#                                 {"instructors": User.objects.filter(role="Instructor"), "instructor": self.valid,
-#                                     "course_name": "course name", "form_name": "create_course"}, follow=True)
-#         self.assertEqual(Course.objects.count(), 1)
-#         self.assertIn('message', response.context)
-#         self.assertEqual(response.context["message"], "The course \"course name\" has been created")
-#
-#
-#     def test_invalid_role(self):
-#         response = self.donkey.post("/configure_course.html",
-#                                 {"instructors" : User.objects.filter(role="Instructor"),"instructor" : self.invalid,
-#                                     "course_name": "course name", "form_name": "create_course"}, follow=True)
-#         self.assertEqual(Course.objects.count(), 0)
-#         self.assertIn('message', response.context)
-#         self.assertEqual(response.context["message"], "Something went wrong when creating the course \"course name\"")
-#
-#     def test_no_instructor_selected(self):
-#         response = self.donkey.post("/configure_course.html",
-#                                     {"instructors": User.objects.filter(role="Instructor"), "instructor": "",
-#                                      "course_name": "course name", "form_name": "create_course"}, follow=True)
-#         self.assertEqual(Course.objects.count(), 0)
-#         self.assertIn('message', response.context)
-#         self.assertEqual(response.context["message"], "Something went wrong when creating the course \"course name\"")
-#
-#     def test_invalid_name(self):
-#         response = self.donkey.post("/configure_course.html",
-#                                 {"instructors" : User.objects.filter(role="Instructor"), "instructor" : self.valid,
-#                                     "course_name": "", "form_name": "create_course"}, follow=True)
-#         self.assertEqual(Course.objects.count(), 0)
-#         self.assertIn('message', response.context)
-#         self.assertEqual(response.context["message"], "Something went wrong when creating the course \"\"")
-#
-#     def test_invalid_form(self):
-#         response = self.donkey.post("/configure_course.html",
-#                                 {"instructors" : User.objects.filter(role="Instructor"), "instructor" : self.valid,
-#                                     "course_name": "course name", "form_name": "fake_form"}, follow=True)
-#         self.assertEqual(Course.objects.count(), 0)
-#         self.assertIn('message', response.context)
-#         self.assertEqual(response.context["message"], "Something went wrong when fetching the form, please try again.")
+    def test_course_two_invalid_user(self):
+        course = CreateCourse.create_course("name", [123, "instructor"])
+        self.assertEqual(course, None)
